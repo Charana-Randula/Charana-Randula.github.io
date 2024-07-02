4b03929ce27c78112c9b4bfff7f1c621bf90e7f9
Charana <108083559+Charana-Randula@users.noreply.github.com>
2024-07-02 15:59:05 +0800

    edit

--- scripts.js
+++ scripts.js
@@ -1,10 +1,12 @@
 const commands = {
     'help': 'Available commands:\nhelp - Display this help message\nabout - Show about information\nprojects - List projects\ncontact - Show contact information\nlinkedin - Open LinkedIn profile\noneth - Open Oneth\'s portfolio\nclear - Clear the terminal',
+    'ls': 'File system under development, use "help"',
     'about': 'Hello! I\'m Charana, an Electrical Engineer with experience in various areas including industrial automation, circuit design, and renewable energy systems. I\'m passionate about leveraging technology to solve real-world problems.',
     'projects': 'Projects:\n1. Industrial Automation System\n2. Renewable Energy Solution\nMore details coming soon...',
     'contact': 'You can contact me via email at: randula@example.com (replace with actual email address)',
     'linkedin': 'Opening LinkedIn profile...',
     'oneth': 'Opening Oneth\'s portfolio...',
+    'kisal': 'Opening Oneth\'s portfolio...',
     'clear': 'clear',
 };
 
@@ -21,7 +23,10 @@ function handleCommand(input) {
         } else if (command === 'oneth') {
             outputElement.innerHTML += `randula@portfolio:~$ ${command}\n${commands[command]}\n`;
             window.open('https://onethk.github.io/onethportfolio/', '_blank');
-        } else {
+        } else if (command === 'kisal') {
+            outputElement.innerHTML += `randula@portfolio:~$ ${command}\n${commands[command]}\n`;
+            window.open('https://www.linkedin.com/in/kisal-fernando-0a2a5120a/', '_blank');
+        } else{
             outputElement.innerHTML += `randula@portfolio:~$ ${command}\n${commands[command]}\n`;
         }
     } else {
